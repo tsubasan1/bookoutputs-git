a1954abdd590f94a7ead77ca245a0906e025668b
tsubasa.omori <tosphere5s@gmail.com>
2020-08-23 00:47:00 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <div class="text-center">
             <h1>Welcome to the OUTPUT world !!</h1>
         </div>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
     </div>
         <h2>List of posts</h2>
 @endsection
