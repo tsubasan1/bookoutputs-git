c201390337878edc0ea02da7c6a678f54bf817d1
tsubasa.omori <tosphere5s@gmail.com>
2020-09-20 12:53:29 +0000

    why_kanmei

--- resources/views/commons/add_change.blade.php
+++ resources/views/commons/add_change.blade.php
@@ -1,11 +1,17 @@
 <div class="form-group">
+    <label>感銘を受けたこと</label>
+    <div class="offset-md-2 col-md-8">
+        <input type="text" class="form-control" name="why">
+    </div>
+</div>
+<div class="form-group">
     <label>今の自分</label>
     <div class="offset-md-2 col-md-8">
         <input type="text" class="form-control" name="now">
     </div>
 </div>
 <div class="form-group">
-    <label>これからの自分</label>
+    <label>目標</label>
     <div class="offset-md-2 col-md-8">
         <input type="text" class="form-control" name="future">
     </div>
@@ -14,12 +20,6 @@
     <label>期待される効果</label>
     <div class="offset-md-2 col-md-8">
         <input type="text" class="form-control" name="effect">
-    </div>
-</div>
-<div class="form-group">
-    <label>なぜそう思ったのか</label>
-    <div class="offset-md-2 col-md-8">
-        <input type="text" class="form-control" name="why">
     </div>
 </div>
 <div class="form-group">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,20 +52,20 @@
                     <td>{{ $book->auther }}</td>
                   </tr>
                   <tr>
+                    <th scope="row">感銘を受けたこと</th>
+                    <td>{{ $change->why }}</td>
+                  </tr>
+                  <tr>
                     <th scope="row">今の自分</th>
                     <td>{{ $change->now }}</td>
                   </tr>
                   <tr>
-                    <th scope="row">これからの自分</th>
+                    <th scope="row">目標</th>
                     <td>{{ $change->future }}</td>
                   </tr>
                   <tr>
                     <th scope="row">期待される効果</th>
                     <td>{{ $change->effect }}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">なぜそう思ったのか</th>
-                    <td>{{ $change->why }}</td>
                   </tr>
                   <tr>
                     <th scope="row">行動した結果</th>
@@ -107,20 +107,20 @@
                           <td>{{ $book->auther }}</td>
                         </tr>
                         <tr>
+                          <th scope="row">感銘を受けたこと</th>
+                          <td>{{ $change->why }}</td>
+                        </tr>
+                        <tr>
                           <th scope="row">今の自分</th>
                           <td>{{ $change->now }}</td>
                         </tr>
                         <tr>
-                          <th scope="row">これからの自分</th>
+                          <th scope="row">目標</th>
                           <td>{{ $change->future }}</td>
                         </tr>
                         <tr>
                           <th scope="row">期待される効果</th>
                           <td>{{ $change->effect }}</td>
-                        </tr>
-                        <tr>
-                          <th scope="row">なぜそう思ったのか</th>
-                          <td>{{ $change->why }}</td>
                         </tr>
                         <tr>
                           <th scope="row">行動した結果</th>
